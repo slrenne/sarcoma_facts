7a713c911167a5a0291113d0cedb60a2d56f8bb8
RENNE Salvatore HUNIMED <srenne@hunimed.eu>
2023-08-18 15:20:46 +0200

    TCGA_MDM2ampl_DDLPS

--- LPS.R
+++ LPS.R
@@ -17,3 +17,7 @@ abline(v = 2, lty = 2)
 axis(1, at = c(1,3), labels = c('high', 'low'))
 legend('topleft', lwd=4, col = c(3,4), legend = c('G3', 'G2'))
 dev.off()
+
+# CNV from MDM2 amplified DDLPS of TCGA firehsose https://www.cbioportal.org/results/oncoprint?comparison_subtab=overlap&comparison_overlapStrategy=Exclude&comparison_selectedGroups=%5B%22Altered%20group%22%2C%22MDM2%22%2C%22HMGA2%22%2C%22GLI1%22%2C%22DDIT3%22%2C%22CDK4%22%5D&plots_horz_selection=%7B%7D&plots_vert_selection=%7B%7D&plots_coloring_selection=%7B%7D&tab_index=tab_visualize&Action=Submit&session_id=64df514f1095f74ff79801da#userSettingsJson={%22clinicallist%22:[{%22stableId%22:%22PROFILED_IN_sarc_tcga_rna_seq_v2_mrna_median_Zscores%22,%22sortOrder%22:null,%22gapOn%22:null},{%22stableId%22:%22CANCER_TYPE_DETAILED%22,%22sortOrder%22:null,%22gapOn%22:null}]}
+
+DDLPS.seg <- read.delim("input/sarc_tcga_segments.seg")
